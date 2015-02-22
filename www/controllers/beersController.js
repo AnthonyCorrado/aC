0398e472f7711f4f77ca57dde32528c44ffbdbb3
AnthonyCorrado <amcstudios@yahoo.com>
2015-02-21 19:29:48 -0800

    restyled beers view

--- www/controllers/beersController.js
+++ www/controllers/beersController.js
@@ -5,35 +5,43 @@ angular.module('BeerController', [])
     $scope.beers = [
         {
             "name": "Sam Adams",
-            "style": "Lager"
+            "style": "Lager",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/spI5OHo77pnjz54/amber.png"
         },
         {
             "name": "90 Minute IPA",
-            "style": "IPA"
+            "style": "IPA",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/16CWe64cxZsWvC3/ipa.png"
         },
         {
             "name": "Young's Double Chocolate Stout",
-            "style": "Stout"
+            "style": "Stout",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/y6ToYBPBAI0FNpd/porter.png"
         },
         {
             "name": "Sculpin IPA",
-            "style": "IPA"
+            "style": "IPA",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/16CWe64cxZsWvC3/ipa.png"
         },
         {
             "name": "Sam Adams",
-            "style": "Lager"
+            "style": "Lager",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/spI5OHo77pnjz54/amber.png"
         },
         {
             "name": "90 Minute IPA",
-            "style": "IPA"
+            "style": "IPA",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/scaBy4snIEefdvc/brown.png"
         },
         {
             "name": "Young's Double Chocolate Stout",
-            "style": "Stout"
+            "style": "Stout",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/y6ToYBPBAI0FNpd/porter.png"
         },
         {
             "name": "Sculpin IPA",
-            "style": "IPA"
+            "style": "IPA",
+            "image": "https://s3.amazonaws.com/uploads.hipchat.com/39979/747064/16CWe64cxZsWvC3/ipa.png"
         },
 
     ];
